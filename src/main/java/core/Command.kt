16cfa8ad05d773ee20ee9c247ee5d6b496611f8a
Leo <leo@ekf.de>
2024-04-10 17:57:37 +0200

    Create Command object with factory method

--- src/main/java/core/Command.kt
+++ src/main/java/core/Command.kt
@@ -2,4 +2,13 @@ package core
 
 class Command(val currentState:String, val currentSymbol:Char, val newSymbol:Char,
               val direction:Int, val newState:String) {
+
+    companion object{
+        fun create(line :String): Command {
+            val list = line.split(" ")
+            return Command(list[0], list[1].toCharArray()[0], list[2].toCharArray()[0],
+                list[3].toInt(), list[4]
+            )
+        }
+    }
 }
